1700729764b2074a5f669c103f16cfcb6f589516
Jimmy <dev.prevenda@saurus.com.br>
2023-02-06 23:44:17 -0300

    Utilizando o StreamReader e suas formas de uso.

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -5,8 +5,21 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        LidandoComFileStreamDiretamente();
+        var enderecoArquivo = "Contas.txt";
+
+        using (var fluxoDeArquivo = new FileStream(enderecoArquivo, FileMode.Open))
+        {
+            var leitor = new StreamReader(fluxoDeArquivo);
+            //var linha = leitor.ReadLine();
+            // var texto = leitor.ReadToEnd();
+            //var numero = leitor.Read();
+            while (!leitor.EndOfStream)
+            {
+                var linha = leitor.ReadLine();
+                Console.WriteLine(linha);
+            }
+        }
         Console.ReadLine();
     }
-    
+
 }
