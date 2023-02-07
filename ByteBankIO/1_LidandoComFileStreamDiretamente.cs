71c957956e863a0bbb53607e3243306511fc3cda
Jimmy <dev.prevenda@saurus.com.br>
2023-02-06 23:31:28 -0300

    Implementando a Classe Partial Program

---
+++ ByteBankIO/1_LidandoComFileStreamDiretamente.cs
@@ -0,0 +1,38 @@
+﻿using ByteBankIO;
+using System.Text;
+
+partial class Program
+{
+    static void LidandoComFileStreamDiretamente()
+    {
+        var enderecoDoArquivo = "contas.txt";
+
+        VerificaArquivo(enderecoDoArquivo);
+    }
+
+    private static void VerificaArquivo(string enderecoDoArquivo)
+    {
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var numeroDeBytesLidos = -1;
+
+            var buffer = new byte[1024];
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, buffer.Length);
+                EscreverBuffer(buffer, numeroDeBytesLidos);
+            }
+            fluxoDoArquivo.Close();
+        }
+    }
+
+    static void EscreverBuffer(byte[] buffer, int bytesLidos)
+    {
+        var utf8 = new UTF8Encoding();
+
+        var texto = utf8.GetString(buffer, 0, bytesLidos);
+        Console.Write(texto);
+
+    }
+}
--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,40 +1,12 @@
 ﻿using ByteBankIO;
 using System.Text;
 
-class Program
+partial class Program
 {
     static void Main(string[] args)
     {
-        var enderecoDoArquivo = "contas.txt";
-
-        VerificaArquivo(enderecoDoArquivo);
-
+        LidandoComFileStreamDiretamente();
         Console.ReadLine();
     }
-
-    private static void VerificaArquivo(string enderecoDoArquivo)
-    {
-        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
-        {
-            var numeroDeBytesLidos = -1;
-
-            var buffer = new byte[1024];
-
-            while (numeroDeBytesLidos != 0)
-            {
-                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, buffer.Length);
-                EscreverBuffer(buffer,numeroDeBytesLidos);
-            }
-            fluxoDoArquivo.Close();
-        }
-    }
-
-    static void EscreverBuffer(byte[] buffer, int bytesLidos)
-    {
-        var utf8 = new UTF8Encoding();
-
-        var texto = utf8.GetString(buffer,0,bytesLidos);
-        Console.Write(texto);
-
-    }
+    
 }
