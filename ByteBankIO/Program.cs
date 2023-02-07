d02becee5509bfb11f971fea5eb03987847180ae
Jimmy <dev.prevenda@saurus.com.br>
2023-02-06 23:01:06 -0300

    implementando UTF8Encoding();

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,14 +1,39 @@
 ﻿using ByteBankIO;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
         var enderecoDoArquivo = "contas.txt";
+        var numeroDeBytesLidos = -1;
         var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
 
         var buffer = new byte[1024];
-        fluxoDoArquivo.Read(buffer, 0, buffer.Length);
+
+        
+
+        while (numeroDeBytesLidos != 0)
+        {
+            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+            EscreverBuffer(buffer);
+
+        }
+
         Console.ReadLine();
+    }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+        var utf8 = new UTF8Encoding();
+
+        var texto = utf8.GetString(buffer);
+        Console.Write(texto);
+        //foreach (var meuByte in buffer)
+        //{
+        //    Console.Write(meuByte);
+        //    Console.Write(" ");
+        //}
+      
     }
 }
