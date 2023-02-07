ee237ce504fb36d0f9d96e12eb8e703e98aa99ce
Jimmy <dev.prevenda@saurus.com.br>
2023-02-06 22:38:12 -0300

    Criando buffer.

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -4,7 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        var enderecoDoArquivo = "contas.txt";
+        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
 
+        var buffer = new byte[1024];
+        fluxoDoArquivo.Read(buffer, 0, buffer.Length);
         Console.ReadLine();
     }
 }
