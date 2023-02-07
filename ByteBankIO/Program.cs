0565e37a96b810205e9d63a53210cb2e7ed10e68
Jimmy <dev.prevenda@saurus.com.br>
2023-02-06 23:23:41 -0300

    Implementando o GetString e rafatorando codigo,

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -6,34 +6,35 @@ class Program
     static void Main(string[] args)
     {
         var enderecoDoArquivo = "contas.txt";
-        var numeroDeBytesLidos = -1;
-        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
 
-        var buffer = new byte[1024];
-
-        
-
-        while (numeroDeBytesLidos != 0)
-        {
-            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-            EscreverBuffer(buffer);
-
-        }
+        VerificaArquivo(enderecoDoArquivo);
 
         Console.ReadLine();
     }
 
-    static void EscreverBuffer(byte[] buffer)
+    private static void VerificaArquivo(string enderecoDoArquivo)
+    {
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var numeroDeBytesLidos = -1;
+
+            var buffer = new byte[1024];
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, buffer.Length);
+                EscreverBuffer(buffer,numeroDeBytesLidos);
+            }
+            fluxoDoArquivo.Close();
+        }
+    }
+
+    static void EscreverBuffer(byte[] buffer, int bytesLidos)
     {
         var utf8 = new UTF8Encoding();
 
-        var texto = utf8.GetString(buffer);
+        var texto = utf8.GetString(buffer,0,bytesLidos);
         Console.Write(texto);
-        //foreach (var meuByte in buffer)
-        //{
-        //    Console.Write(meuByte);
-        //    Console.Write(" ");
-        //}
-      
+
     }
 }
