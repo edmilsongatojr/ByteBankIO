a652d5152aacc184871af2877d121628ae394863
Jimmy <dev.prevenda@saurus.com.br>
2023-02-07 21:39:13 -0300

    Utilizando o StreamWriter

---
+++ ByteBankIO/3_CriandoArquivo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+partial class Program
+{
+    static void CriarArquivo()
+    {
+        var caminhoNovoArquivo = "contasExportadas.csv";
+
+        using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        {
+            var contaComoString = "456, 7892, 4763.40, Gustavo Santos";
+
+            var encoding = Encoding.UTF8;
+
+            var bytes = encoding.GetBytes(contaComoString);
+            fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+        }
+    }
+
+    static void CriarArquivoComWriter()
+    {
+        var caminhoNovoArquivo = "contasExportadas.csv";
+
+        using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        using (var escritor = new StreamWriter(fluxoDeArquivo))
+        {
+            escritor.Write("456,2135,451.0,Pedrão");
+        }
+    }
+}
+
