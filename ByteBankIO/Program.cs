f658ce89907f7a7b71afb1c77e5e4721c29bdaef
Jimmy <dev.prevenda@saurus.com.br>
2023-02-07 21:11:03 -0300

    Fazendo a leitura e tratando os campos do txt

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -16,10 +16,34 @@ partial class Program
             while (!leitor.EndOfStream)
             {
                 var linha = leitor.ReadLine();
-                Console.WriteLine(linha);
+                var contaCorrente = ConverterStringParaContaCorrente(linha);
+
+                var msg = $"Conta numero {contaCorrente.Numero}, Agencia: {contaCorrente.Agencia}, Saldo: {contaCorrente.Saldo} ";
+                Console.WriteLine(msg);
             }
         }
         Console.ReadLine();
     }
 
+    static ContaCorrente ConverterStringParaContaCorrente(string linha)
+    {
+        var campos = linha.Split(',');
+        var agencia = campos[0];
+        var numero = campos[1];
+        var saldo = campos[2].Replace('.',',');
+        var nomeTitular = campos[3];
+
+        var agenciaComInt = int.Parse(agencia);
+        var numeroComInt = int.Parse(numero);
+        var saldoComDouble = double.Parse(saldo);
+
+        var titular = new Cliente();
+        titular.Nome = nomeTitular; ;
+
+        var resultado = new ContaCorrente(agenciaComInt, numeroComInt);
+        resultado.Depositar(saldoComDouble);
+        resultado.Titular = titular;
+
+       return resultado;
+    }
 }
